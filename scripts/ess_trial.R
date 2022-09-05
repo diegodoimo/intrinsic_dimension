5306f373544e95ea037f98e393c845f2acb68862
DiegoD <doimo.diego@gmail.com>
2022-09-05 17:15:32 +0200

    u

--- scripts/ess_trial.R
+++ scripts/ess_trial.R
@@ -1,11 +1,22 @@
-library(intrinsicDimension)
+#source('/home/diego/ricerca/past/IDestimators/intrinsicDimension/R/ESS.R')
 
+#source('/home/diego/ricerca/past/IDestimators/intrinsicDimension/R/make_vec.R')
+
+#source('/home/diego/ricerca/past/IDestimators/intrinsicDimension/R/DimEst.R')
+#library(yaImpute)
+#source('/home/diego/ricerca/past/IDestimators/intrinsicDimension/R/pointwise.R')
+#tests on syntetic data
+library(intrinsicDimension)
 library(reticulate)
 library(tidyverse)
 library(here)
 np <- import("numpy")
+# data reading
+getwd()
+twonn_isolet <- t(np$load("../datasets/real/cifar_cat_11x11.npy"))
 
-#tests on syntetic data
+print(dim(twonn_isolet))
+
 
 data_files<-list.files("../datasets/syntetic/csv")
 results_path<-"./results/syntetic_datasets/ess/"
@@ -23,24 +34,29 @@ for(i in 1:length(data_files)) {
     for (fraction in list(1, 2, 4,8, 16, 32, 64, 128, 256, 512)){
       n <- ndata%/%fraction
       print(fraction)
-      for (nrep in (1:fraction)){
+      for (nrep in (1:1)){
         s <-sample(nrow(mat),size=n,replace=FALSE)
         X = mat[s,]
+        print(dim(X))
+        #data <- swissRoll3Sph(300, 300)
+        data_matrix<-as.matrix(sapply(X, as.numeric))
+        #print(is.matrix(data_matrix))
 
+        #print(data_matrix)
         essPointwiseDimEst <- asPointwiseEstimator(essLocalDimEst, neighborhood.size=30, indices=NULL)
         #print(is.matrix(X))
-        ess.pw.res <- essPointwiseDimEst(  as.matrix(sapply(X, as.numeric))  )
-        id_val<-mean(ess.pw.res$dim.est)
+        ess.pw.res <- essPointwiseDimEst(data_matrix)
+        id_val<-ess.pw.res$dim.est
 
         #id<-essLocalDimEst(X, ver = 'a', d = 1)
         #id_val<-id$dim.est
+        print(length(id_val))
+        print(mean(id_val))
+        print(median(id_val))
+
         de<-c(n, id_val)
         ids<-rbind(ids, de)
 
         }
       }
-
-    #filename<-paste0(results_path, substring(data_files[i], 1, nchar(data_files[i])-4), ".txt")
-    #write.table(ids, filename, append = FALSE, sep = " ", dec = ".",
-    #            row.names = FALSE, col.names = FALSE)
-  }
+    }
